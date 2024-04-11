57b5656779c53c3acb4210b293a62a74b82340d8
austinli356 <94568713+austinli356@users.noreply.github.com>
2024-04-10 22:23:53 -0700

    added date filtering and fixed bugs

--- Controllers/MedicalTransactionsController.cs
+++ Controllers/MedicalTransactionsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OAinternship.Models;
+using System;
+using System.Net;
 
 
 namespace OAinternship.Controllers
@@ -18,8 +20,10 @@ namespace OAinternship.Controllers
             return View();
         }
         [HttpGet]
-        public ActionResult<IEnumerable<VMedicalTransaction>> GetMedicalTransactions(int pageIndex = 0, int pageSize = 10, int? patientId = null)
+        public ActionResult<IEnumerable<VMedicalTransaction>> GetMedicalTransactions(int pageIndex = 0, int pageSize = 10, int? patientId = null, string firstname = "", string lastname = "", int max = 100000, int min = 0, string nurse = "", string doctor = "", string start = "2000-01-01", string end = "3000-01-01")
         {
+            DateOnly startDate = DateOnly.ParseExact(start, "yyyy-MM-dd");
+            DateOnly endDate = DateOnly.ParseExact(end, "yyyy-MM-dd");
             var patientName = "";
             var medicalTransactionsQuery = _context.VMedicalTransactions.AsQueryable();
 
@@ -29,6 +33,9 @@ namespace OAinternship.Controllers
                 patientName = patient.FirstName + " " + patient.LastName;
                 medicalTransactionsQuery = medicalTransactionsQuery.Where(x => x.PatientId == patientId);
             }
+
+            medicalTransactionsQuery = medicalTransactionsQuery.Where(x => (x.PatientFirstName.Contains(firstname) && x.PatientLastName.Contains(lastname) && (x.NurseFirstName + x.NurseLastName).Contains(nurse) && (x.DoctorFirstName + x.DoctorLastName).Contains(doctor)&&(x.TotalCost.Value >= min && x.TotalCost <= max)&&(x.TransactionDate>=startDate && x.TransactionDate<=endDate)));
+
             var totalRecords = medicalTransactionsQuery.Count();
             var medicalTransactions = medicalTransactionsQuery.Skip((pageIndex - 1) * pageSize)
                                 .Take(pageSize)
